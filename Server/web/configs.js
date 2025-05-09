7cade514443da18b6f4e9753fee43098194c0b92
lylxiaole <742923150@qq.com>
2025-05-09 15:28:36 +0800

    123

--- Server/web/configs.js
+++ Server/web/configs.js
@@ -1,1 +1,3 @@
-window.__globalConfig={projectName:"super pro",apiAddr:"/",websocketAddr:"/"};
+window.__globalConfig = { projectName: 'super pro', apiAddr: '/', websocketAddr: '/' }
+window.__globalConfig.apiAddr = 'http://' + window.location.hostname + ':63013/'
+window.__globalConfig.websocketAddr = 'ws://' + window.location.hostname + ':63012/chat'
